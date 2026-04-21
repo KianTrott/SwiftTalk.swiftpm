a3ef04b3ad8cadc855eab2ada1a849ea71bf0864
Kian <ktrotter8454@stu.d214.org>
2026-04-21 11:04:45 -0500

    added NavigationLink

--- ContentView.swift
+++ ContentView.swift
@@ -3,34 +3,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        
-        Text("SwiftTalk")
-            .bold()
-            .offset(x: 0, y: -300)
-            .font(.largeTitle)
-        
-        
-        Text("Welcome")
-            .font(.title)
-            .offset(x: -350, y: -100)
-            .rotationEffect(.degrees(22))
-        
-        Text("Bienvenido")
-            .font(.title)
-            .offset(x: -160, y: -200)
-            .rotationEffect(.degrees(345))
-        
-        Text("Empfang")
-            .font(.title)
-            .offset(x: 350, y: -100)
-            .rotationEffect(.degrees(345))
-        
-        Text("Bienvenu")
-            .font(.title)
-            .offset(x: -160, y: -200)
-            .rotationEffect(.degrees(22))
-        
-    
-       
+        NavigationStack{
+            
+            Text("SwiftTalk")
+                .bold()
+                .offset(x: 0, y: -300)
+                .font(.largeTitle)
+            
+            
+            Text("Welcome")
+                .font(.title)
+                .offset(x: -350, y: -100)
+                .rotationEffect(.degrees(22))
+            
+            Text("Bienvenido")
+                .font(.title)
+                .offset(x: -160, y: -200)
+                .rotationEffect(.degrees(345))
+            
+            Text("Empfang")
+                .font(.title)
+                .offset(x: 350, y: -100)
+                .rotationEffect(.degrees(345))
+            
+            Text("Bienvenu")
+                .font(.title)
+                .offset(x: -160, y: -200)
+                .rotationEffect(.degrees(22))
+            
+            
+        }
     }
 }
