4a16289b7a7a2755d039caa84d4dd6a693e58ff8
Arjan Sethi <arjansethi0514@gmail.com>
2026-04-21 10:54:05 -0500

    added title and subtitles

--- ContentView.swift
+++ ContentView.swift
@@ -2,14 +2,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            
-                NavigationLink("English", destination: ESView())
-                .font(.system(size: 50, weight: .regular, design: .default))
-                .foregroundStyle(.black)
-                .underline()
-            
-            
-        }
+        
+        
+        Text("SwiftTalk")
+            .bold()
+            .offset(x: 0, y: -300)
+            .font(.largeTitle)
+        
+        
+        Text("Welcome")
+            .font(.title)
+            .offset(x: -350, y: -100)
+            .rotationEffect(.degrees(22))
+        
+        Text("Bienvenido")
+            .font(.title)
+            .offset(x: -160, y: -200)
+            .rotationEffect(.degrees(345))
+        
+        Text("Empfang")
+            .font(.title)
+            .offset(x: 350, y: -100)
+            .rotationEffect(.degrees(345))
+        
+        Text("Bienvenu")
+            .font(.title)
+            .offset(x: -160, y: -200)
+            .rotationEffect(.degrees(22))
+        
+    
+       
     }
 }
