8f579944f091bc44c136d62df83ef0c877251551
Arjan Sethi <arjansethi0514@gmail.com>
2026-04-23 10:24:17 -0500

    added title page and subtitle too english view

--- EnglishView.swift
+++ EnglishView.swift
@@ -8,8 +8,23 @@ import SwiftUI
 
 struct EnglishView: View {
     var body: some View {
+        Text("Select a language")
+            .font(.system(size: 90, weight: .regular, design: .default))
+            .offset(x: 0, y: -570)
         
+            Divider()
+            .background(Color.black)
+            .offset(x: 0, y: -600 )
         
+        VStack {
+            Text("Translate")
+                .bold()
+                .font(.system(size: 60, weight: .regular, design: .default))
+                .offset(x: -230, y: -570)
+            
+            
+            
+        }
         
     }
 }
