af9c5749ae594ac04efaef7c455cd3117aa0e051
Kian <ktrotter8454@stu.d214.org>
2026-04-23 10:23:30 -0500

    Finished default language buttons

---
+++ FrenchView.swift
@@ -0,0 +1,15 @@
+//
+//  FrenchView.swift
+//  SwiftTalk
+//
+//  Created by Kian J. Trotter on 4/23/26.
+//
+import SwiftUI
+
+struct FrenchView: View {
+    var body: some View {
+        
+        
+        
+    }
+}
