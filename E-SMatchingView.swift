e065a84736d177636c4981ac506ca9541eb625a7
Kian <ktrotter8454@stu.d214.org>
2026-04-27 10:41:35 -0500

    added the home button in the spanishg matching

--- E-SMatchingView.swift
+++ E-SMatchingView.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
-
 struct ESMatchingView: View {
     var body: some View {
-        
-        
-        
+        VStack {
+            Text("Matching")
+                .font(.system(size: 90, weight: .regular, design: .default))
+                .offset(x: 0, y: -230)
+            
+            Divider()
+                .background(Color.black)
+                .offset(x: 0, y: -250)
+            
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            NavigationLink(destination: ContentView()) {
+                                Image(systemName: "house")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                            }
+                            Spacer(minLength: 900)
+                        }
+                    }
+                }
+                .navigationBarBackButtonHidden()
+        }
     }
 }
-
