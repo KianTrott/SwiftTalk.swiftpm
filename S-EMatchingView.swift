c6c25c68366e5f1bf7270b3255bb060370ceb30f
Arjan Sethi <arjansethi0514@gmail.com>
2026-04-29 10:36:08 -0500

    Created all text in Spanish to English matching view

--- S-EMatchingView.swift
+++ S-EMatchingView.swift
@@ -10,6 +10,130 @@ import SwiftUI
 
 struct SEMatchingView: View {
     var body: some View {
+        VStack {
+            Text("Emparejar")
+                .font(.system(size: 90, weight: .regular, design: .default))
+                .offset(x: 0, y: -320)
+            
+            Divider()
+                .background(Color.black)
+                .offset(x: 0, y: -360)
+        }
+        HStack{
+            
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+            .padding()
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+        }
+        HStack{
+            
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+            .padding()
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+        }
+        HStack{
+            
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .foregroundStyle(.white)
+                }
+            })
+            .padding()
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 150, height: 150)
+                        .border(Color.black, width: 4)
+                        .foregroundStyle(.white)
+                }
+            })
+        }
         
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            NavigationLink(destination: SpanishView()) {
+                                Image(systemName: "house")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                            }
+                            Spacer(minLength: 900)
+                        }
+                    }
+                }
+                .navigationBarBackButtonHidden()
     }
 }
